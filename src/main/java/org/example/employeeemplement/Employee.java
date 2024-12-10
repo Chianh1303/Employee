71a1dbf8feddfdbd9b00127c78831c3adff33aaa
Chianh1303 <chianhn567@gmai.com>
2024-12-10 13:58:38 +0700

    Hien thi danh sach

--- src/main/java/org/example/employeeemplement/Employee.java
+++ src/main/java/org/example/employeeemplement/Employee.java
@@ -1,5 +1,79 @@
 package org.example.employeeemplement;
 
 public class Employee {
+private int id;
+private String name;
+private int age;
+private String position;
+private String departments;
+private String salary;
 
+    public Employee(int id, String name, int age, String position, String departments, String salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.departments = departments;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(String departments) {
+        this.departments = departments;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", position='" + position + '\'' +
+                ", departments='" + departments + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,13 +1,45 @@
+
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="org.example.employeeemplement.Employee" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<%
+    List<Employee> employees = new ArrayList<>();
+    employees.add(new Employee(1, "Nguyễn Chí Anh", 20, "Tổng giám đốc","Phòng giám đốc","9999999$"));
+    employees.add(new Employee(2, "Nguyễn Chí Khánh", 19, "Trưởng phòng vệ sinh","Cả công ty","1000$"));
+    employees.add(new Employee(3, "Nguyễn Chí Sâm", 19, "Trưởng phòng bảo vệ","Bãi gửi xe và cả công ty","2000$"));
+    employees.add(new Employee(4, "Nguyễn Chí Trang", 19, "Nhân viên quèn","Đáy xã hội","999$"));
+
+    request.setAttribute("employees", employees);
+%>
+<h1 style="text-align: center;">Quản lý nhân viên</h1>
+<table>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>age</th>
+        <th>position</th>
+        <th>departments</th>
+        <th>salary</th>
+    </tr>
+    <c:forEach var="employees" items="${employees}"  >
+        <tr>
+            <td>${employees.id}</td>
+            <td>${employees.name}</td>
+            <td>${employees.age}</td>
+            <td>${employees.position}</td>
+            <td>${employees.departments}</td>
+            <td>${employees.salary}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
